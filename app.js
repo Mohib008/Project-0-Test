a5fa835ba7fc422df0b0f82792a3112afe61698d
Mohibullah Arsala <mohib-arsala@Mohib-MacBook-Pro.local>
2021-02-03 12:49:34 -0500

    updates from Macpro

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
+let quotes = [
+  {
+    quote: "I see dead people.",
+    movie: "The Sixth Sense",
+    year: 1999,
+    rating: "PG-13"
+  }, {
+    quote: "May the force be with you.",
+    movie: "Star Wars: Episode IV - A New Hope",
+    year: 1977,
+    rating: "PG"
+  }, {
+    quote: "You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya, punk?",
+    movie: "Dirty Harry",
+    year: 1971,
+    rating: "R"
+  }, {
+    quote: "You had me at 'hello.'",
+    movie: "Jerry Maguire",
+    year: 1996,
+    rating: "R"
+  }, {
+    quote: "Just keep swimming. Just keep swimming. Swimming, swimming, swiming.",
+    movie: "Finding Nemo",
+    year: 2003,
+    rating: "G"
+  }
+];
+
 d3.select(".remove")
-  .on("click", function(){
-      d3.selectAll(".note")
-        .remove();
-  });
+  .on("click", click => d3.selectAll(".note")
+        .remove());
 
 d3.select(".lucky")
-  .on("click", function(){
+  .on("click", click =>
       d3.selectAll(".note")
         .style("font-size", function(){
             return Math.random() * 100 + "px";
-        });
-  });
+        })
+  );
 
   let input = d3.select("input");
   let preview = d3.select(".preview");
@@ -36,3 +63,13 @@ function setPreview(val) {
 };
 
 
+d3.select(".quotes")
+     .style("list-style", "none")
+     .selectAll("li")
+     .data(quotes)
+     .enter()
+     .append("li")
+     .text(d => '"' + d.quote + '" - ' + d.movie + ' (' + d.year  + ')')
+     .style("margin", "20px")
+     .style("padding", "20px")
+     .style("font-size", d => d.quote.length < 25 ? "2em" : "1em");
